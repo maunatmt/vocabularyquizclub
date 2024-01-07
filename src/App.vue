a0cd9d481ca43264043a8a137dc8bf37ecf2d0b2
maunatmt <pop.orange.1@gmail.com>
2024-01-07 16:21:47 +0200

    Change ads setting.

--- src/App.vue
+++ src/App.vue
@@ -31,18 +31,6 @@
       let route = this.$route;
       this.setMeta(route);
 
-      const adsScript = document.createElement(`script`);
-      adsScript.async = true;
-      adsScript.crossOrigin = `anonymous`;
-      adsScript.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9126377173560430`;
-
-      const inlineScript = document.createElement(`script`);
-      inlineScript.type = `text/javascript`;
-      inlineScript.text = `(adsbygoogle = window.adsbygoogle || []).push({});`;
-
-      document.head.appendChild(adsScript);
-      document.head.appendChild(inlineScript);
-
       // let externalScript = document.createElement('script')
       // externalScript.setAttribute('src', 'https://consent.cookiebot.com/uc.js?cbid=2788f7e2-e0f0-48bc-a6b4-18571ed6bbf8')
       // document.head.appendChild(externalScript)
